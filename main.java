679f5ce1e3b184e8296a9a45445e7e4855b81ea3
muhammadsafarudinfirmani <muhammadsafarudinfirmani@gmail.com>
2026-04-20 14:28:40 +0700

    menampilkan output

--- main.java
+++ main.java
@@ -4,11 +4,13 @@ public class main {
     public static void main(String[] args) {
         produk p = new produk( "indomie goreng",  5000,  100, "makanan");
 
-        System.out.println(p.stok);
-        //System.out.println(p.harga);
-        //System.out.println(p.nama);
-        //System.out.println(p.kategori);
-
-        p.tampilkaninfo();
+        System.out.println(p.getHarga());
+        System.out.println(p.getNama());
+        System.out.println(p.getStok());
+        p.setHarga(10000);
+        p.setStok(150);
+        System.out.println(p.getHarga());
+        System.out.println(p.getNama());
+        System.out.println(p.getStok());
     }
 }
