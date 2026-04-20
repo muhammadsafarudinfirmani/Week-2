39ef2cb780fab0eeb181b211e53fcd884ee24c58
muhammadsafarudinfirmani <muhammadsafarudinfirmani@gmail.com>
2026-04-20 13:56:01 +0700

    menambahkan method get

--- Base/produk.java
+++ Base/produk.java
@@ -3,21 +3,27 @@ package Base;
 public class produk {
     //atribut
     private String nama;
-    String kategori;
-    protected double harga;
-    public int stok;
+    private double harga;
+    private int stok;
 
     //Constructor
     public produk(String nama, double harga, int stok, String kategori) {
         this.nama = nama;
         this.harga = harga;
         this.stok = stok;
-        this.kategori = kategori;
+    }
+    public double getHarga(){
+        return harga;
+    }
+    public double getStok(){
+        return stok;
+    }
+    public String getNama(){
+        return nama;
     }
 
     public void tampilkaninfo(){
         System.out.println("Produk:" + nama);
-        System.out.println("Kategori:" + kategori);
         System.out.println("Harga:" + harga);
         System.out.println("Stok:" + stok);
         System.out.println("--------------------");
